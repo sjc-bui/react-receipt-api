14be4a84678f5cf2694f1bc1d7c5b544b0687625
sjc-bui <bui@setjapan.co.jp>
2022-09-19 22:24:25 +0900

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Popular from '../components/Popular'
+import Veggie from '../components/Veggie'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <Popular/>
+      <Veggie/>
+    </div>
+  )
+}
+
+export default Home
